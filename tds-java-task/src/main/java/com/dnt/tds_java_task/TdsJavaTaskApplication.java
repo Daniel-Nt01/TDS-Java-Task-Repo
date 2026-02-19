9a9708f4b203b6b3b88e284d72acc3dcdb6dda7b
Daniel-Nt01 <dannt2nd@gmail.com>
2026-02-19 19:02:38 +0000

    Initial set up of project

---
+++ tds-java-task/src/main/java/com/dnt/tds_java_task/TdsJavaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.dnt.tds_java_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdsJavaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdsJavaTaskApplication.class, args);
+	}
+
+}
